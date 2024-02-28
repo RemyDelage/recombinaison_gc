d0f7ee89e519e886336835e4717028c6eaf4cdfe
Rémy Delage <rdelage@genossh.genouest.org>
2024-02-28 11:10:50 +0100

    add code documentation

--- scripts/get_ncbi_data.py
+++ scripts/get_ncbi_data.py
@@ -1,6 +1,7 @@
-#### GET_NCBI_DATA
-## This script allows to directly download the genomic fasta and gff files from the NCBI Database 
-#  only thanks to this accession number
+####################### GET_NCBI_DATA #########################################
+# This script allows to directly download the genomic fasta and gff files from 
+# the NCBI Database only thanks to this accession number
+###############################################################################
 
 
 
@@ -14,7 +15,8 @@ import argparse
 
 def is_module_installed(module_name):
     '''
-    This finction checks if the necessary modules for run this script are already installed
+    This finction checks if the necessary modules for run this script are 
+    already installed
 
     Parameters
     ----------
@@ -29,7 +31,7 @@ def is_module_installed(module_name):
 
 # Function to install a module using pip
 def install_module(module_name):
-    """
+    '''
     Install a Python module using pip.
 
     Parameters
@@ -39,72 +41,95 @@ def install_module(module_name):
     Returns
     -------
         None
-    """
-    try:
+    '''
+    try: # Try to intsall the module
         import pip
-        pip.main(['install', module_name])
+        pip.main(['install', module_name]) # Module installation
     except Exception as e:
-        print(f"Error installing module {module_name}: {e}")
+        print(f"Error installing module {module_name}: {e}") 
+        # Error message if the module installation failed
 
 # List of required modules
 required_modules = ['ftplib', 'os', 'wget', 'argparse']
 
 # Check and install required modules
 installed_modules = []
-for module in required_modules:
-    if not is_module_installed(module):
+for module in required_modules: # Check if the module is installed
+    if not is_module_installed(module): # If the module is not installed
         print(f"The module {module} is not installed. Installing...")
-        install_module(module)
-        installed_modules.append(module)
+        install_module(module) # Install the module
+        installed_modules.append(module) # Add the module to the list of installed modules
     else:
         installed_modules.append(module)
 
-## Creation of the arguments for the command line
+# Creation of the arguments for the command line
+## Create the parser
 parser = argparse.ArgumentParser(description = 'Download fasta and gff files from the NCBI GeneBank database')
+## Add the accession number to the parser
 parser.add_argument('-i', '--input', type = str, 
                     help = 'The accession number of the data')
 
 def download_files(accession_number: str):
+    '''
+    This function allows to download the genomic fasta and gff files from 
+    the NCBI database.
 
-    accession_num = accession_number.replace("_","")
-    extension = accession_num[-2:]
-    accession_no_extension = accession_num[:-2]
-    accession_first_letters = accession_num[0:3]
+    Parameters
+    ----------
+        accession_number : str
+            The accession_number of the data to download
+
+    Returns
+    -------
+        None
+    '''
+
+    accession_num = accession_number.replace("_","") # Remove the underscore
+    extension = accession_num[-2:] # Store the extension of the accession number
+    accession_no_extension = accession_num[:-2] # Remove the extension
+    accession_first_letters = accession_num[0:3] # Keep the first letters
     accession_part = [accession_no_extension[i:i+3] for i in range(0, len(accession_no_extension), 3)]
+    # Partitionning the accession number like : AAA/xxx/xxx/xxx/xxx
 
-    ftp_folder_path = f"/genomes/all/{'/'.join(accession_part)}/"    
+    # Define the path to the folder containing the data on FTP server
+    ftp_folder_path = f"/genomes/all/{'/'.join(accession_part)}/"   
     
+    # Connexion to the NCBI FTP server
     ftp = ftplib.FTP("ftp.ncbi.nlm.nih.gov")
     ftp.login()
     
-    ftp.cwd(ftp_folder_path)
-    folders = ftp.nlst()
+    ftp.cwd(ftp_folder_path) # Go to the folder that contains the data
+    folders = ftp.nlst() # Get the list of the folders of the path.
     
+    # Get the folder that matches the accession number
     matching_folders = [folder for folder in folders if folder.startswith(accession_number)]
-  
+
+    # Error message if no matching folders are found
     if not matching_folders: 
         print("No files found. Check the accession number or if data exists on the NCBI GeneBank database.")
         return
     
-    selected_folder = matching_folders[0]
+    selected_folder = matching_folders[0] # Select the first folder that mathes with the accession number
     ftp.cwd(selected_folder)  # Change to the selected folder
     
     files = ftp.nlst()  # Get the list of files in the selected folder
 
+    # Download the files
     for file in files:
         if file.endswith("_genomic.fna.gz") or file.endswith("_genomic.gff.gz"):
-            if not file.endswith("_from_genomic.fna.gz"): 
-                file_url = f"ftp://{ftp.host}{ftp.pwd()}/{file}"
-                print(f"Downloading genomic file : {file}")
-                wget.download(file_url)
-                print(" -> Download complete")
+            if not file.endswith("_from_genomic.fna.gz"): # Download only the genomic files
+                file_url = f"ftp://{ftp.host}{ftp.pwd()}/{file}" # get the URL of the file
+                print(f"Downloading genomic file : {file}") # Print the downoaded file name
+                wget.download(file_url) # Download the file
+                print(" -> Download complete") # Inform the user that the download is complete
     
+    # Close the FTP server connexion and not display errors messages
     try:
-        ftp.quit()
-    except ftplib.error_temp as e:
+        ftp.quit() 
+    except ftplib.error_temp as e: 
         pass
 
-
+# Parse the arguments
 args = parser.parse_args()
 if args.input:
     download_files(args.input)
