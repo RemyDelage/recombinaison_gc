ca79761aa8a744dc85d0df3b5c2f392285a40fdc
Rémy Delage <rdelage@cl1n019.genouest.org>
2024-03-05 14:49:58 +0100

    update orthofinder script

--- scripts/orthofinder.sh
+++ scripts/orthofinder.sh
@@ -7,6 +7,7 @@
 
 # Intitialization environment
 . /local/env/envorthofinder-2.5.2.sh
+. /local/env/envpython-3.9.5.sh
 
 # Variables
 out_dir="/home/genouest/cnrs_umr6553/rdelage/results/01_orthofinder"
@@ -17,10 +18,8 @@ for file in "$in_dir"/*.fasta.gz; do
 	gunzip "$file"
 done
 
-# Command line
-orthofinder -o "$out_dir" -f "$in_dir" -d
+# Use Orthofinder on the primary transcripts only
+for f in *fasta ; do python /home/genouest/cnrs_umr6553/rdelage/scripts/primary_transcript.py $f ; done
 
-# Compress the fasta file at the end of the orthofinder execution
-for file in "$in_dir"/*.fasta; do
-        gzip "$file"
-done
+# Command line
+orthofinder -t 16 -a 16 -S diamond -M dendroblast -o "$out_dir" -f "$in_dir"
