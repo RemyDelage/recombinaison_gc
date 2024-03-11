a0369412ff0cae829cddacecdcce3519b92218da
Rémy Delage <rdelage@genossh.genouest.org>
2024-03-11 20:19:11 +0100

    correction working directory

--- scripts/cactus.sh
+++ scripts/cactus.sh
@@ -8,13 +8,16 @@
 data_dir="/home/genouest/cnrs_umr6553/rdelage/data/"
 jobStorePath="$data_dir""tmp"
 inputSeqFile="$data_dir""GenomicSequencesFile_arabidopsis.txt"
-out_dir="/home/genouest/cnrs_umr6553/rdelage/results/02_cactus/"
+out_dir="/home/genouest/cnrs_umr6553/rdelage/results/"
+cactus_dir="02_cactus/"
 outputHalFile="arabidopsis_alignment.hal"
 outputMafFile="arabidopsis_alignment.maf"
 
+# Create the output directory
+mkdir -p "$out_dir$cactus_dir"
 
 # Script execution (with Singularity)
-singularity exec --bind "$data_dir":/mnt --bind "$out_dir":/mnt --pwd $PWD:/mnt cactus.sif cactus --consCores 16 "$jobStorePath" "$inputSeqFile" "$out_dir$outputHalFile"
+singularity exec --bind "$data_dir":/mnt --bind "$out_dir" --pwd "$data_dir" cactus.sif cactus --consCores 16 "$jobStorePath" "$inputSeqFile" "$out_dir$outputHalFile"
 
 # Convert hal files to maf files for make it readable by many tools (HAL Tool ; use with Singularity)
 #singularity exec cactus.sif hal2maf "$out_dir$outputHalFile" "$out_dir$outputMafFile"
