f4a151995596f5bb4c7d9b20057cab0074f4fab5
Rémy Delage <rdelage@genossh.genouest.org>
2024-04-05 17:02:48 +0200

    correction syntax error

--- scripts/cactus.sh
+++ scripts/cactus.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=cactus
-#SBTACH --cpus-per-task=8
-#SBTACH --mem=250G
-#SBATCH --output=cactus_all_genomes_20240404.log
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=350G
+#SBATCH --output=cactus_all_genomes_20240405.log
+
 
 #######################################################################
 # Sequences alignments on reference genome using (Progressive) Cactus #
@@ -14,17 +15,14 @@ out_dir="/home/genouest/cnrs_umr6553/rdelage/results/03_cactus/"
 species_dir="arabidopsis/"
 jobStorePath="${out_dir}tmp4"
 inputSeqFile="${data_dir}GenomicSequencesFile_arabidopsis.txt"
+sif_file="/scratch/rdelage/cactus.sif"
 outputHalFile="arabidopsis_all_genomes_alignment.hal"
-outputMafFile="arabidopsis_all_genomes_alignment.maf"
 
 # Create the output directory
-mkdir -p "$out_dir$cactus_dir"
+mkdir -p "${out_dir}/cactus_results"
 
 # Set the maximum memory available on the cluster (recommanded by documentation : https://github.com/ComparativeGenomicsToolkit/cactus/blob/master/doc/progressive.md#running-on-a-cluster)
 max_memory="3T"
 
 # Script execution (with Singularity)
-singularity exec --bind "$data_dir":/mnt --bind "$out_dir" --pwd "$data_dir" cactus.sif cactus --consCores 2 --maxMemory "$max_memory" "$jobStorePath" "$inputSeqFile" "$out_dir$outputHalFile"
-
-# Convert hal files to maf files for make it readable by many tools (HAL Tool ; use with Singularity)
-singularity exec cactus.sif hal2maf "$out_dir$outputHalFile" "$out_dir$outputMafFile"
+singularity exec --bind "$data_dir":/mnt --bind "$out_dir" --pwd "$data_dir" "$sif_file" cactus --consCores 2 --maxMemory "$max_memory" "$jobStorePath" "$inputSeqFile" "$out_dir$outputHalFile"
