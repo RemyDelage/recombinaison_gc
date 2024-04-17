e4a51a34e0d313e4b123a140cfca2f7d74967244
Rémy Delage <rdelage@genossh.genouest.org>
2024-04-17 11:19:30 +0200

    syntax errors correction

--- scripts/cactus.sh
+++ scripts/cactus.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=cactus
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=350G
-#SBATCH --output=cactus_all_genomes_20240405.log
+#SBATCH --output=cactus_hal2maf_20240417.log
 
 
 #######################################################################
@@ -16,7 +16,7 @@ species_dir="arabidopsis/"
 jobStorePath="${out_dir}tmp"
 inputSeqFile="${data_dir}GenomicSequencesFile_arabidopsis.txt"
 sif_file="/scratch/rdelage/cactus.sif"
-outputHalFile="arabidopsis_genomes_alignment.hal"
+outputHalFile="arabidopsis_all_genomes_alignment.hal"
 outputMafFile="arabidopsis_genomes_alignment.maf.gz"
 
 # Create the output directory
@@ -25,8 +25,11 @@ mkdir -p "${out_dir}/cactus_results"
 # Set the maximum memory available on the cluster (recommanded by documentation : https://github.com/ComparativeGenomicsToolkit/cactus/blob/master/doc/progressive.md#running-on-a-cluster)
 max_memory="3T"
 
-# cactus-hal2maf execution (with Singularity)
-singularity exec --bind "$data_dir":/mnt --bind "$out_dir" --pwd "$data_dir" "$sif_file" cactus-hal2maf "$out_dir$outputHalFile" "$out_dir$outputMafFile" --refGenome GCF_000001735.4_Arabidopsis_thaliana --noAncestors --chunkSize 500000 --batchCores 2 --filterGapCausingDupes --maxMemory "$max_memory" "$jobStorePath" "$inputSeqFile" 
+# cactus execution (with Singularity)
+singularity exec --bind "$data_dir":/mnt --bind "$out_dir" --pwd "$data_dir" "$sif_file" cactus --consCores 2 --maxMemory "$max_memory" "$jobStorePath" "$inputSeqFile" "$out_dir$outputHalFile"
+
+# cactus_hal2maf execution (convert hal file into maf file)
+singularity exec --bind "$data_dir":/mnt --bind "$out_dir" --pwd "$data_dir" "$sif_file" cactus-hal2maf --refGenome GCF_000001735.4_Arabidopsis_thaliana --noAncestors --chunkSize 500000 --batchCores 2 --filterGapCausingDupes "$jobStorePath" "$out_dir$outputHalFile" "$out_dir$outputMafFile"
 
 # Alignment inspection
 singularity exec "$sif_file" halStats "$outputHalFile"
