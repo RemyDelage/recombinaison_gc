48b61c776098ac2d278c4a8e9c1986accfd8df22
Rémy Delage <rdelage@genossh.genouest.org>
2024-04-22 16:57:33 +0200

    script for WGAbed execution --> must to be automatized

---
+++ scripts/wgabed.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=wgabed
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=150G
+#SBATCH --output=wgabed_20240422.log
+
+# Load Python 2 environment for use this script
+. /local/env/envpython-2.7.15.sh
+
+# Variables
+data_dir="/home/genouest/cnrs_umr6553/rdelage/results/03_cactus/cactus_results/April18/"
+results_dir="/home/genouest/cnrs_umr6553/rdelage/results/04_wgabed/"
+scripts_dir="/home/genouest/cnrs_umr6553/rdelage/scripts/05_wgabed/WGAbed/"
+fasta_file_ref="/home/genouest/cnrs_umr6553/rdelage/results/02_genes_fasta/02a_gffutils/GCF_000001735.4_Arabidopsis_thaliana_genomic.fa"
+input_file=$data_dir"arabidopsis_genomes_alignment.maf.gz"
+uncompressed_input_file=$data_dir"arabidopsis_genomes_alignment.maf"
+species_names_file=$scripts_dir"data/species_list.txt"
+
+# Chromosomes ID list
+chromosomes=($(grep -n ">" "$fasta_file_ref" | awk -F ">" '{print $2}' | awk '{print $1}'))
+
+# Modify the name of the species of the MAF file
+gunzip -c "$input_file"
+sed -i 's/GCF_000001735.4_Arabidopsis_thaliana/GCF_000001735_Arabidopsis_thaliana/g' "$uncompressed_input_file"
+sed -i "s/GCF_000004255.2_arabidopsis_lyrata/GCF_000004255_arabidopsis_lyrata/g" "$uncompressed_input_file"
+sed -i "s/GCF_000309985.2_Brassica_rapa/GCF_000309985_Brassica_rapa/g" "$uncompressed_input_file"
+gzip "$uncompressed_input_file"
+
+# Cheks if the bgzip package is installed; install it otherwize
+if ! command -v bgzip &> /dev/null; then
+    pip install bgzip
+fi
+
+# Script execution (for each chromosome)
+for i in "${chromosomes[@]}"; do
+	python "$scripts_dir"maf_to_bed_v2.py -i "$input_file" -r GCF_000001735_Arabidopsis_thaliana -c "$i" -s "$species_names_file" | sort -k1,1 -k2,2n | bgzip -c > "$results_dir"arabidopsis_"$i".wga.bed.gz
+done
