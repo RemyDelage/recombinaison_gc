7b51ee0280e526f2828f6b05be4dbed60095f871
Rémy Delage <rdelage@genossh.genouest.org>
2024-04-23 10:23:25 +0200

    syntax errors correction

--- scripts/wgabed.sh
+++ scripts/wgabed.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=wgabed
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=150G
-#SBATCH --output=wgabed_20240422.log
+#SBATCH --output=wgabed_20240423.log
 
 # Load Python 2 environment for use this script
 . /local/env/envpython-2.7.15.sh
@@ -20,11 +20,11 @@ species_names_file=$scripts_dir"data/species_list.txt"
 chromosomes=($(grep -n ">" "$fasta_file_ref" | awk -F ">" '{print $2}' | awk '{print $1}'))
 
 # Modify the name of the species of the MAF file
-gunzip -c "$input_file"
+gunzip -c "$input_file" > "$uncompressed_input_file"
 sed -i 's/GCF_000001735.4_Arabidopsis_thaliana/GCF_000001735_Arabidopsis_thaliana/g' "$uncompressed_input_file"
-sed -i "s/GCF_000004255.2_arabidopsis_lyrata/GCF_000004255_arabidopsis_lyrata/g" "$uncompressed_input_file"
-sed -i "s/GCF_000309985.2_Brassica_rapa/GCF_000309985_Brassica_rapa/g" "$uncompressed_input_file"
-gzip "$uncompressed_input_file"
+sed -i 's/GCF_000004255.2_arabidopsis_lyrata/GCF_000004255_arabidopsis_lyrata/g' "$uncompressed_input_file"
+sed -i 's/GCF_000309985.2_Brassica_rapa/GCF_000309985_Brassica_rapa/g' "$uncompressed_input_file"
+gzip -f "$uncompressed_input_file" 
 
 # Cheks if the bgzip package is installed; install it otherwize
 if ! command -v bgzip &> /dev/null; then
