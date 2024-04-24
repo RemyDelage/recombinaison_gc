2c8b31c1a048e46431d39d9d3896a4f901400a10
Rémy Delage <rdelage@genossh.genouest.org>
2024-04-24 09:59:14 +0200

    correction of syntax errors

--- scripts/wgabed.sh
+++ scripts/wgabed.sh
@@ -12,8 +12,9 @@ data_dir="/home/genouest/cnrs_umr6553/rdelage/results/03_cactus/cactus_results/A
 results_dir="/home/genouest/cnrs_umr6553/rdelage/results/04_wgabed/"
 scripts_dir="/home/genouest/cnrs_umr6553/rdelage/scripts/05_wgabed/WGAbed/"
 fasta_file_ref="/home/genouest/cnrs_umr6553/rdelage/results/02_genes_fasta/02a_gffutils/GCF_000001735.4_Arabidopsis_thaliana_genomic.fa"
-input_file=$data_dir"arabidopsis_genomes_alignment.maf.gz"
+input_file=$data_dir"arabidopsis_alignment.maf.gz"
 uncompressed_input_file=$data_dir"arabidopsis_genomes_alignment.maf"
+updated_input_file=$data_dir"arabidopsis_genomes_alignment.maf.gz"
 species_names_file=$scripts_dir"data/species_list.txt"
 
 # Chromosomes ID list
@@ -22,7 +23,7 @@ chromosomes=($(grep -n ">" "$fasta_file_ref" | awk -F ">" '{print $2}' | awk '{p
 # Modify the name of the species of the MAF file
 gunzip -c "$input_file" > "$uncompressed_input_file"
 sed -i 's/GCF_000001735.4_Arabidopsis_thaliana/GCF_000001735_Arabidopsis_thaliana/g' "$uncompressed_input_file"
-sed -i 's/GCF_000004255.2_arabidopsis_lyrata/GCF_000004255_arabidopsis_lyrata/g' "$uncompressed_input_file"
+sed -i 's/GCF_000004255.2_Arabidopsis_lyrata/GCF_000004255_Arabidopsis_lyrata/g' "$uncompressed_input_file"
 sed -i 's/GCF_000309985.2_Brassica_rapa/GCF_000309985_Brassica_rapa/g' "$uncompressed_input_file"
 gzip -f "$uncompressed_input_file" 
 
@@ -33,5 +34,5 @@ fi
 
 # Script execution (for each chromosome)
 for i in "${chromosomes[@]}"; do
-	python "$scripts_dir"maf_to_bed_v2.py -i "$input_file" -r GCF_000001735_Arabidopsis_thaliana -c "$i" -s "$species_names_file" | sort -k1,1 -k2,2n | bgzip -c > "$results_dir"arabidopsis_"$i".wga.bed.gz
+	python "$scripts_dir"maf_to_bed_v2.py -i "$updated_input_file" -r GCF_000001735_Arabidopsis_thaliana -c "$i" -s "$species_names_file" | sort -k1,1 -k2,2n | bgzip -c > "$results_dir"arabidopsis_"$i".wga.bed.gz
 done
