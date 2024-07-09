1d9da5fd653228866a87b99395329a8f98507e1b
Rémy Delage <rdelage@genossh.genouest.org>
2024-07-09 10:17:10 +0200

    Update environment loading for use VCFtools

--- scripts/vcf_alleles_counts.sh
+++ scripts/vcf_alleles_counts.sh
@@ -2,15 +2,16 @@
 #SBATCH --job-name=vcftools_counts
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=150G
-#SBATCH --output=vcftools_counts_20240529.log
+#SBATCH --output=vcftools_counts_sorghum_20240709.log
+
+# Environment
+. /local/env/envvcftools-0.1.16.sh
 
 # Variables
 directory="/home/genouest/cnrs_umr6553/rdelage/data/"
 output_dir="/home/genouest/cnrs_umr6553/rdelage/results/vcftools/"
-input_vcf="${directory}Arabidopsis_thaliana_1001genomes.pop_sfs.no_indels.recode.vcf.gz"
-
-
+input_vcf="${directory}Sorghum_bicolor_Lozano2021.pop_sfs.no_indels.recode.vcf.gz"
 
 # Script execution
-vcftools --gzvcf "$input_vcf" --counts --out "${output_dir}Arabidopsis_thaliana_snp_vcf"
-vcftools --gzvcf "$input_vcf" --freq --out "${output_dir}Arabidopsis_thaliana_freq"
+vcftools --gzvcf "$input_vcf" --counts --out "${output_dir}Sorghum_bicolor_snp_vcf"
+vcftools --gzvcf "$input_vcf" --freq --out "${output_dir}Sorghum_bicolor_freq"
