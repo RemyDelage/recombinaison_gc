10a36f7898d3bed0dbb006af542d52d62c42f0f1
remy delage <redelage@mr088055.localdomain>
2024-07-11 10:48:05 +0200

    Create functions and documentation

--- scripts/gradients.R
+++ scripts/gradients.R
@@ -31,14 +31,11 @@ load_packages()
 read_gff <- function(species){
   if(species == "Arabidopsis_thaliana"){
     gff = readRDS("gff_rho_Arabidopsis_thaliana_1001genomes.rds")
-  }
-  if(species == "Oryza_sativa"){
+  }else if(species == "Oryza_sativa"){
     gff = readRDS("gff_rho_Oryza_sativa_Wang2018.rds")
-  }
-  if(species == "Sorghum_bicolor"){
+  }else if(species == "Sorghum_bicolor"){
     gff = readRDS("gff_rho_Sorghum_bicolor_Lozano2021.rds")
-  }
-  if(species == "Populus_tremula"){
+  }else if(species == "Populus_tremula"){
     gff = readRDS("gff_rho_Populus_tremula_Liu2022.rds")
   }
   cds = gff[which(gff$feature == "CDS" & gff$nb_exons < 15 & gff$rank < 15),]
@@ -51,13 +48,23 @@ read_gff("Arabidopsis_thaliana")
 read_gff("Oryza_sativa")
 
 
-### Recombination gradient
+#'@title Recombination gradient plot
+#' 
+#' @description Allows to plot the recombination gradients at CDS level 
+#' (previously defined into the read GFF function) with the gradients for genes 
+#' with sizes from 1 to 14 exons. The median recombination gradient is also plotted.
+#' The plot will be stored into a PNG file format.
+#' 
+#' @param species (character) : The species which we wants to read the data
+#' @param xlim (by default : c(1, 14)) : Scale for the x-axis.
+#' @param ylim (by default : c(0, 1)) : Scale for the y-axis
+#'
+#' @return None
 
-recombination_gradient_plot <- function(species, frontsize = 16, dotsize = 0.2, linesize = 1.5){
+recombination_gradient_plot <- function(species, xlim = c(1, 14), ylim = c(0, 1)){
   species_plot_title <- gsub("_", " ", species)
 
   cds <- get(paste0("cds_", species))
-  typeof(cds)
   
   recomb_rank = aggregate(weighted.mean.rho ~ rank + nb_exons, cds , median)
   recomb_rank$nb_exons = as.factor(recomb_rank$nb_exons)
@@ -66,9 +73,9 @@ recombination_gradient_plot <- function(species, frontsize = 16, dotsize = 0.2, 
     scale_color_viridis_d() +
     geom_line() +
     geom_point() +
-    xlim(1, 14) +
-    ylim(0, 1) +
-    xlab("CDS part rank") + ylab("Median recombination rate (ρ/kb)") +
+    xlim(xlim) +
+    ylim(ylim) +
+    xlab("Gene length (exons)") + ylab("Median recombination rate (ρ/kb)") +
     labs(colour = "# exons", title  = species_plot_title) +
     scale_alpha_manual(values = c(0.4, 1)) +
     guides(alpha = "none")+
@@ -84,100 +91,65 @@ recombination_gradient_plot <- function(species, frontsize = 16, dotsize = 0.2, 
 }
 
 recombination_gradient_plot("Arabidopsis_thaliana")
+recombination_gradient_plot("Oryza_sativa", ylim = c(0.045, 0.125))
 
+#'@title GC gradient plot
+#' 
+#' @description Allows to plot the GC content gradients at CDS level 
+#' (previously defined into the read GFF function) with the gradients for genes 
+#' with sizes from 1 to 14 exons. GC content corresponds to that at the 3rd codon position.
+#' The median GC content gradient is also plotted.
+#' The plot will be stored into a PNG file format.
+#' 
+#' @param species (character) : The species which we wants to read the data
+#' @param xlim (by default : c(1, 14)) : Scale for the x-axis.
+#' @param ylim (by default : c(0, 1)) : Scale for the y-axis
+#'
+#' @return None
 
+gc_gradient <- function(species, xlim = c(1,14), ylim = c(0,1), legend = TRUE){
+  species_plot_title = gsub("_", " ", species)
+  cds <- get(paste0("cds_", species))
+  
+  recomb_rank = aggregate(weighted.mean.rho ~ rank + nb_exons, cds , median)
+  recomb_rank$nb_exons = as.factor(recomb_rank$nb_exons)
+  
+  gc_rank <- aggregate(gc3 ~ rank + nb_exons, cds, median)
+  gc_rank$nb_exons <- as.factor(gc_rank$nb_exons)
+  summary(gc_rank)
+  
+  p2 = ggplot(gc_rank, aes(x = rank, y = gc3, group = nb_exons, colour = nb_exons)) +
+    scale_color_discrete(breaks = unique(recomb_rank$nb_exons), 
+                         labels = unique(recomb_rank$nb_exons), 
+                         aesthetics = "colour") +
+    scale_colour_viridis_d(option = "inferno") +
+    geom_line() +
+    geom_point() +
+    xlim(1, 14) +
+    ylim(0, 1) +
+    xlab("Gene length (exons)") + ylab("GC content (3rd codon positions)") +
+    scale_alpha_manual(values = c(0.4, 1)) +
+    guides(alpha = "none")+
+    theme_classic()
+  
+  df2_avg = aggregate(gc3 ~ rank , gc_rank , median)
+  
+  p2 = p2 + geom_line(data = df2_avg, aes(x = rank, y = gc3, group = "black", colour = "black"), colour = "black", linewidth = 1.5)
+  
+  if(!legend){
+    p2 = p2 + theme(legend.position = "none")
+  } else {
+    p2 = p2 + 
+      labs(colour = "# exons", title = species_plot_title) +
+      theme(plot.title = element_text(hjust = 0.5))
+  }
+  
+  
+  ggsave(paste0(species,"_gc3_gradient.png"), p2, width = 8, height = 8)
+}
 
-
-
-### GC3 gradient
-
-gc_rank <- aggregate(gc3 ~ rank + nb_exons, cds, median)
-summary(gc_rank)
-gc_rank$nb_exons <- as.factor(gc_rank$nb_exons)
-
-p2 = ggplot(gc_rank, aes(x = rank, y = gc3, group = nb_exons, colour = nb_exons)) +
-  scale_color_discrete(breaks = unique(recomb_rank$nb_exons), 
-                       labels = unique(recomb_rank$nb_exons), 
-                       aesthetics = "colour") +
-  scale_colour_viridis_d(option = "inferno") +
-  geom_line() +
-  geom_point() +
-  xlim(1, 14) +
-  ylim(0, 1) +
-  xlab("CDS part rank") + ylab("GC3 rate") +
-  labs(colour = "# exons") +
-  scale_alpha_manual(values = c(0.4, 1)) +
-  guides(alpha = "none")+
-  theme_classic()+
-  theme(legend.position = "none")
-
-p2
-
-df2_avg = aggregate(gc3 ~ rank , gc_rank , median)
-
-p2 = p2 + geom_line(data = df2_avg, aes(x = rank, y = gc3, group = "black", colour = "black"), colour = "black", linewidth = 1.5)
-p2
-
-# ---- ORYZA SATIVA ---- 
-
-gff_oryza <- readRDS("gff_rho_Oryza_sativa_Wang2018.rds")
-head(gff_oryza)
-cds_oryza = gff_oryza[which(gff_oryza$feature == "CDS" & gff_oryza$nb_exons < 15 & gff_oryza$rank < 15),]
-
-### Recombination gradient
-
-recomb_rank_oryza = aggregate(weighted.mean.rho ~ rank + nb_exons, cds_oryza, median)
-recomb_rank_oryza$nb_exons = as.factor(recomb_rank_oryza$nb_exons)
-
-fontsize = 16
-dotsize = 0.2
-linesize = 1.5
-
-p1_oryza = ggplot(recomb_rank_oryza, aes(x = rank, y = weighted.mean.rho, group = nb_exons, colour = nb_exons)) +
-  scale_color_viridis_d() +
-  geom_line() +
-  geom_point() +
-  xlim(1, 14) +
-  ylim(0, 1)+
-  xlab("CDS part rank") + ylab("Median recombination rate (ρ/kb)") +
-  labs(colour = "# exons", title  = "Oryza sativa") +
-  scale_alpha_manual(values = c(0.4, 1)) +
-  guides(alpha = "none")+
-  theme_classic()+
-  theme(plot.title = element_text(hjust = 0.5))
-
-df_avg_oryza = aggregate(weighted.mean.rho ~ rank , recomb_rank_oryza , median)
-
-p1_oryza = p1_oryza + geom_line(data = df_avg_oryza, aes(x = rank, y = weighted.mean.rho, group = "black", colour = "black"), colour = "black", linewidth = 1.5)
-p1_oryza
-
-
-### GC3 gradient
-
-gc_rank_oryza <- aggregate(gc3 ~ rank + nb_exons, cds_oryza, median)
-gc_rank_oryza$nb_exons <- as.factor(gc_rank_oryza$nb_exons)
-
-p2_oryza = ggplot(gc_rank_oryza, aes(x = rank, y = gc3, group = nb_exons, colour = nb_exons)) +
-  scale_color_discrete(breaks = unique(recomb_rank_oryza$nb_exons), 
-                       labels = unique(recomb_rank_oryza$nb_exons), 
-                       aesthetics = "colour") +
-  scale_colour_viridis_d(option = "inferno") +
-  geom_line() +
-  geom_point() +
-  xlim(1, 14) +
-  ylim(0, 1) +
-  xlab("CDS part rank") + ylab("GC3 rate") +
-  labs(colour = "# exons") +
-  scale_alpha_manual(values = c(0.4, 1)) +
-  guides(alpha = "none")+
-  theme_classic()
-
-df2_avg_oryza = aggregate(gc3 ~ rank , gc_rank_oryza , median)
-
-p2_oryza = p2_oryza + geom_line(data = df2_avg_oryza, aes(x = rank, y = gc3, group = "black", colour = "black"), colour = "black", linewidth = 1.5)
-p2_oryza
-
-
+gc_gradient("Arabidopsis_thaliana")
+gc_gradient("Oryza_sativa", legend = FALSE)
 
 
 
