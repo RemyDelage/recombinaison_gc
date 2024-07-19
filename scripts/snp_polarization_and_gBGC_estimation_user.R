07d0081d1d94baf18b94a1d9d80deb83abb8e9c0
remy delage <redelage@mr088055.localdomain>
2024-07-19 10:02:07 +0200

    User script for SNP polarization and gBGC computations

---
+++ scripts/snp_polarization_and_gBGC_estimation_user.R
@@ -0,0 +1,7 @@
+species <- "Sorghum_bicolor"
+chromosome_num <- 1
+gff_file <- "gff_rho_Sorghum_bicolor_Lozano2021.rds"
+genomic_level <- "exon"
+counts_file <- "Sorghum_bicolor_Lozano2021.1.frq.count"
+
+source("snp_polarization_and_gBGC_estimation.R")
